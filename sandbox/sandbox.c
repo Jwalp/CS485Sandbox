1230e0a086857188598c6edcbcf7b736f3d944e9
jwalp <jwalp@njit.edu>
2026-03-03 15:44:23 -0500

    Made basic skeleton

---
+++ sandbox/sandbox.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char* argv[]){
+	if (argc < 2) {
+ 	   printf("Usage: sandbox <create|launch|destroy>\n");
+    	return 1;
+	}
+
+	if (strcmp(argv[1], "create") == 0) {
+    		if (argc < 4) {
+        		printf("Usage: sandbox create <name> <rootfs>\n");
+        	return 1;
+    		}
+    		char *name   = argv[2];
+    		char *rootfs = argv[3];
+    		printf("Creating sandbox '%s' with rootfs '%s'\n", name, rootfs);
+    	} 
+	else if (strcmp(argv[1], "launch") == 0) {
+    		if (argc < 4) {
+        		printf("Usage: sandbox launch <name> <program>\n");
+        	return 1;
+    		}
+    		char *name   = argv[2];
+    		char *program = argv[3];
+    		printf("Launching sandbox '%s' with program '%s'\n", name, program);
+	} else if (strcmp(argv[1], "destroy") == 0) {
+    		if (argc < 4) {
+        		printf("Usage: sandbox destroy <name>\n");
+        	return 1;
+    		}
+    		char *name   = argv[2];
+    		printf("Destroying '%s'\n", name);
+	} else {
+    		printf("Unknown command: %s\n", argv[1]);
+	}
+}
