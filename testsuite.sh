2dc15c4fb5522c3e7093112d797c641edb576078
pac27-cloud <pac27@njit.edu>
2026-03-05 17:54:26 -0500

    finished test suite

--- testsuite.sh
+++ testsuite.sh
@@ -1,33 +1,61 @@
-echo 
-
+cd "cs485_sandbox_test_suite/bin/public"
 case "$1" in
 	"milestone1")
 		echo "Running Test Suite for milestone 1"
-		cd "cs485_sandbox_test_suite/bin/public/milestone1" # this may not be the correct relative root. test and change
+		cd "milestone1"
 		binaries=(*)
 		echo "${binaries[@]}"
 		for binary in "${binaries[@]}"; do
-			stdout=$(./"$binary")
-			if grep -q "FAIL" <<< "$stdout"; then
+			stdout=$("./$binary")
+			if echo "$stdout" | grep -q "FAIL"; then
 				echo "FAILED $binary"
 				echo "$stdout"
-			else
+			elif echo "$stdout" | grep -q "PASS"; then
 				echo "PASSED $binary"
+			else
+				echo "ERROR ENCOUNTERED IN $binary"
 			fi
 			echo -e "\n"
 		done
 		;;
 	"milestone2")
-		echo "Compiling Test Suite for milestone 2"
-		# WIP
+		echo "Running Test Suite for milestone 2"
+		cd "milestone2"
+		binaries=(*)
+		echo "${binaries[@]}"
+		for binary in "${binaries[@]}"; do
+			stdout=$("./$binary")
+			if echo "$stdout" | grep -q "FAIL"; then
+				echo "FAILED $binary"
+				echo "$stdout"
+			elif echo "$stdout" | grep -q "PASS"; then
+				echo "PASSED $binary"
+			else
+				echo "ERROR ENCOUNTERED IN $binary"
+			fi
+			echo -e "\n"
 		;;
 	"final")
-		echo "Compiling Test Suite for final"
-		# WIP
+		echo "Running Test Suite for final"
+                cd "milestone2"
+                binaries=(*)
+                echo "${binaries[@]}"
+                for binary in "${binaries[@]}"; do
+                        stdout=$("./$binary")
+                        if echo "$stdout" | grep -q "FAIL"; then
+                                echo "FAILED $binary"
+                                echo "$stdout"
+                        elif echo "$stdout" | grep -q "PASS"; then
+                                echo "PASSED $binary"
+                        else
+                                echo "ERROR ENCOUNTERED IN $binary"
+                        fi
+                        echo -e "\n"
 		;;
 	"")
-		echo "Usage: ./testsuite.sh (milestone1|milestone2|final)"
-		echo "Ensure that you are running this within your sandbox"
+		echo "INFO: Usage: ./testsuite.sh (milestone1|milestone2|final)"
+		echo "INFO: Ensure that you are running this within your sandbox"
+		echo "INFO: Ensure that have precompiled the binaries before running"
 		;;
 	*)
 		echo "Could not find test suite"
